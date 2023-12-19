023c27f71ac0748877b1090582e8f88e0191c950
Mario <mariotrabert370@gmail.com>
2023-12-19 13:04:06 +0100

    create Post Detail Page

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,45 @@
+import { getAllPostSlugs, getPostData } from '@/lib/posts'
+
+type Props = {
+  params: { slug: string }
+}
+
+type PostData = {
+  title: string
+  date: string
+  contentHtml: string
+}
+
+export async function generateMetadata({ params }: Props) {
+  const postData: PostData = await getPostData(params.slug)
+  console.log(params.slug)
+  return {
+    title: postData.title,
+  }
+}
+
+// -< Post >-
+export default async function Post({ params }: Props) {
+  const postData: PostData = await getPostData(params.slug)
+
+  return (
+    <>
+      {/* Post Title */}
+      <h1 className='font-extrabold text-3xl mb-1'>{postData.title}</h1>
+
+      <div className='text-gray-500 font-medium mb-5'>
+        <div>{postData.date}</div>
+      </div>
+
+      {/* Post Content */}
+      <div
+        className='text-gray-600'
+        dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+      />
+    </>
+  )
+}
+
+/* TIP: dangerouslySetInnerHTML is a React feature that allows you to render HTML that comes from an external source as if it were regular JSX. It replaces innerHTML used by Javascript.
+Here we are rendering the HTML that comes from the markdown file thanks to remark (remark converted the markdown into HTML)
+*/
