434a97c007d8398ce8e5de94afe8c4f9caf8768c
Mario <mariotrabert370@gmail.com>
2023-12-19 12:56:35 +0100

    add daisyui and tailwindcss typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
+  //daisyui config
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#15803d",
+          secondary: "#84cc16",
+          accent: "#facc15",
+          neutral: "#ffffff",
+          "base-100": "#f3f4f6",
+          info: "#ffffff",
+          success: "#a3e635",
+          warning: "#f59e0b",
+          error: "#dc2626",
+        },
+      },
+    ],
+  },
+};
+export default config;
