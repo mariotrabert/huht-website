7c5ae692c139a3d009564910b6f6c7b195eab0ee
Mario <mariotrabert370@gmail.com>
2023-12-19 17:31:03 +0100

    use daisyui components

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,58 +1,107 @@
-import Link from "next/link";
+'use client'
+
 import Image from "next/image";
 import logo from "@/public/2011VDWZuechterLogo200.png";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
-export function NavBar() {
+const NavBar = () => {
   return (
-    <>
-      <nav className="bg-emerald-900 py-4 px-48">
-        <div className="container mx-auto grid grid-cols-6">
-          <div className=" text-white text-lg font-semibold">
-            <Link href="/">
-              <Image
-                src={logo}
-                alt="DW Logo"
-                className="w-auto h-4/5 max-h-24"
-              ></Image>
-            </Link>
+    
+      <nav className="navbar bg-primary text-primary-content lg:sticky lg:top-0">
+        <div className="navbar-start">
+          {/* TODO change to drawer */}
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Item 3</a>
+              </li>
+            </ul>
           </div>
+          <Link href="/" className="btn btn-ghost text-xl">
+            <Image
+              src={logo}
+              alt="DW Logo"
+              className="w-auto h-4/5 max-h-24 mr-3"
+            ></Image>
+            Von der Huht
+          </Link>
+        </div>
 
-          <div className="col-start-2 col-end-6 grid grid-rows-2 gap-2 justify-start">
-            <div>
-              <h1 className="text-white font-semibold">
-                Deutscher Wachtelhund-Zwinger von der Huht
-              </h1>
-            </div>
-            {/* <div className="grid grid-flow-col"> */}
-            <div className="flex justify-between">
-              <Link href="/" className="text-white hover:text-gray-300">
-                Home
-              </Link>
-              <Link href="/blog" className="text-white hover:text-gray-300">
-                Aktuelles
-              </Link>
-              <Link href="/about" className="text-white hover:text-gray-300">
-                Über uns
-              </Link>
-              <Link href="/fotos" className="text-white hover:text-gray-300">
-                Galerie
-              </Link>
-              <Link href="/zucht" className="text-white hover:text-gray-300">
-                Zucht
-              </Link>
-              {/* <div></div> */}
-            </div>
-          </div>
-          <div className="col-span-1 grid grid-rows-2 gap-2">
-            <div></div>
-            <div className="col-span-1 text-right justify-end">
-              <Link href="#" className="text-white hover:text-gray-300">
-                Login
-              </Link>
-            </div>
-          </div>
+        <div className="navbar-center hidden lg:flex">
+          <NavLinks />
+        </div>
+
+        <div className="navbar-end">
+          <button className="btn">
+            <Link href="/login">Login</Link>
+          </button>
         </div>
       </nav>
-    </>
   );
-}
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Home", href: "/" },
+    { label: "Blog", href: "/blog" },
+    { label: "Über uns", href: "/ueber" },
+    { label: "Galerie", href: "/fotos" },
+    { label: "Zucht", href: "/zucht" },
+  ];
+  return (
+    // menu menu-horizontal px-1
+    <ul className="menu menu-horizontal space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classnames({
+              "nav-link": true,
+              "!text-slate-100 !font-bold": link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NavBar;
