9de95619e1a3f4d5b841aea906c9a2b025a05f3b
Mario <mariotrabert370@gmail.com>
2024-01-13 20:18:54 +0100

    increase navbar z-index, add icon to login button

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,77 +1,92 @@
-'use client'
+"use client";
 
-import Image from "next/image";
 import logo from "@/public/2011VDWZuechterLogo200.png";
+import classnames from "classnames";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import classnames from "classnames";
 
 const NavBar = () => {
   return (
-    
-      <nav className="navbar bg-primary text-primary-content z-[1] lg:sticky lg:top-0">
-        <div className="navbar-start">
-          {/* TODO change to drawer */}
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+    <nav className="navbar bg-primary text-primary-content z-[50] lg:sticky lg:top-0">
+      <div className="navbar-start">
+        {/* TODO change to drawer */}
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
-          <Link href="/" className="btn btn-ghost text-xl">
-            <Image
-              src={logo}
-              alt="DW Logo"
-              className="w-auto h-4/5 max-h-24 mr-3"
-            ></Image>
-            Von der Huht
-          </Link>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
         </div>
+        <Link href="/" className="btn btn-ghost text-xl">
+          <Image
+            src={logo}
+            alt="DW Logo"
+            className="w-auto h-4/5 max-h-24 mr-3"
+          ></Image>
+          Von der Huht
+        </Link>
+      </div>
 
-        <div className="navbar-center hidden lg:flex">
-          <NavLinks />
-        </div>
+      <div className="navbar-center hidden lg:flex">
+        <NavLinks />
+      </div>
 
-        <div className="navbar-end">
-          <button className="btn">
-            <Link href="/login">Login</Link>
+      <div className="navbar-end">
+        <Link href="/login">
+          <button className="btn">Login
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"
+              />
+            </svg>
+
           </button>
-        </div>
-      </nav>
+        </Link>
+      </div>
+    </nav>
   );
 };
 
