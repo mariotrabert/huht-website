a9e350e171670a231cebd0595baf559d460c6050
Mario <mariotrabert370@gmail.com>
2024-01-18 15:54:04 +0100

    add AuthProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "@/app/components/NavBar";
+import AuthProvider from "./api/auth/Provider";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -19,13 +20,15 @@ export default function RootLayout({
   return (
     <html data-theme="huhtTheme">
       <body className={inter.variable}>
-        <NavBar />
-        <div id="content-wrapper" className="flex flex-col min-h-screen">
-          <div className="flex-grow px-12 py-5 sm:px-24 md:px-30 lg:px-40 lg:py-10">
-            {children}
+        <AuthProvider>
+          <NavBar />
+          <div id="content-wrapper" className="flex flex-col min-h-screen">
+            <div className="flex-grow px-12 py-5 sm:px-24 md:px-30 lg:px-40 lg:py-10">
+              {children}
+            </div>
           </div>
-        </div>
-        <Footer />
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
