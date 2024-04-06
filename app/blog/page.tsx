ef56f71c12cef2f9fd4179b0dcd0cd3931706aaa
Mario <mariotrabert370@gmail.com>
2024-04-06 13:46:08 +0200

    update blog page to use posts from database

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,35 +1,30 @@
-import Link from 'next/link'
+import Link from "next/link";
+import prisma from "@/prisma/client";
 
-import { getSortedPostsData } from '@/lib/posts'
-
-type AllPostsData = {
-  date: string
-  title: string
-  slug: string
-  description: string
-}[]
-
-export default function BlogPage() {
-  const allPostsData: AllPostsData = getSortedPostsData()
+const BlogPage = async () => {
+  const posts = await prisma.post.findMany();
 
   return (
-    <>
-      <section>
-        <h1>Blog</h1>
-        <ul>
-          {allPostsData.map(({ slug, date, title, description }) => (
-            <li key={slug}>
-              <div className='font-medium mb-1 mt-5'>
-                <Link href={`/blog/${slug}`}>{title}</Link>
-              </div>
-              <p>{description}</p>
-              <small className='text-gray-500 font-medium'>
-                <div>{date}</div>
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </>
-  )
-}
+    <section>
+      <h1>Blog</h1>
+      <Link href="/blog/new">
+        <button className="btn btn-primary mt-5">Neuer Beitrag</button>
+      </Link>
+      <ul>
+        {posts.map(({ id, date, title, description }) => (
+          <li key={id}>
+            <div className="font-medium mb-1 mt-5">
+              <Link href={`/blog/${id}`}>{title}</Link>
+            </div>
+            <p>{description}</p>
+            <small className="text-gray-500 font-medium">
+              <div>{date.toDateString()}</div>
+            </small>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default BlogPage;
