0e1592270daf834020edef6de66579b35da74620
Mario <mariotrabert370@gmail.com>
2024-04-06 13:43:33 +0200

    add german and english locales

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ['de-DE', 'en'],
+    defaultLocale: 'de-DE',
+  },
   images: {
     remotePatterns: [
       {
